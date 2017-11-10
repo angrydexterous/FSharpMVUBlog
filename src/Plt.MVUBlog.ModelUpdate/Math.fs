154323de44f933ad5ca4f1eb63abbc1e2144e7ac
William Coletto <william.coletto@prolucid.ca>
2017-11-10 17:12:14 -0500

    fixing a factoring 1 bug

--- src/Plt.MVUBlog.ModelUpdate/Math.fs
+++ src/Plt.MVUBlog.ModelUpdate/Math.fs
@@ -1,7 +1,8 @@
 namespace Plt.MVUBlog.ModelUpdate
 
-module Math = 
-    // Taken from 
+open Aether
+module Math =
+    // Taken from
     // https://jeremybytes.blogspot.ca/2016/07/getting-prime-factors-in-f-with-good.html
     let primeFactors (n:int64) =
         let rec getFactor num proposed acc =
@@ -11,4 +12,7 @@ module Math =
                 getFactor (num/proposed) proposed (proposed::acc)
             else
                 getFactor num (proposed+1L) acc
-        if n > 0L then getFactor n 2L [] else [0L]
+        match n with
+        | x when x <= 0L -> [0L]
+        | 1L -> [1L]
+        | _ -> getFactor n 2L []
